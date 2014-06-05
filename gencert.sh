c74e8650945585a2178519d2ec666df11da52121
wus <swunderlin@gmail.com>
2014-06-05 22:19:15 +0200

    working https server

--- gencert.sh
+++ gencert.sh
@@ -24,3 +24,11 @@ echo "new files:"
 echo "   key.pem"
 echo "   req.pem"
 echo "   cert.pem"
+
+# alternative method
+# http://talkweb.eu/full-https-rest-server-in-node-js/
+#openssl genrsa -out privatekey.pem 1024 
+#openssl req -new -key privatekey.pem -out certrequest.csr 
+#openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
+
+
--- https.js
+++ https.js
@@ -1,20 +1,14 @@
 #!/usr/bin/env node
 
-const crypto = require('crypto'),
-  fs = require("fs"),
-  http = require("http");
+var https = require('https');
+var fs = require('fs');
 
-var privateKey = fs.readFileSync('key.pem').toString();
-var certificate = fs.readFileSync('cert.pem').toString();
-
-var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
-
-var handler = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
 };
 
-var server = http.createServer();
-//server.setSecure(credentials);
-server.addListener("request", handler);
-server.listen(8000);
+var a = https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(8000);
