70ecdad0496f5279d5cded0895030465952ad211
wus <swunderlin@gmail.com>
2014-06-05 22:22:31 +0200

    added comments

--- https.js
+++ https.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
+// https and filesystem modules
 var https = require('https');
 var fs = require('fs');
 
+// cert options, must create certs with gencert.sh first
 var options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
 };
 
+// starting server on port 8000
 var a = https.createServer(options, function (req, res) {
   res.writeHead(200);
   res.end("hello world\n");
